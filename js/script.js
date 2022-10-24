8672fc0202f24c1af1f06a99e78a14b40a0d8f2d
SHAHBAZ HUSSAIN <90305433+shahbazhussaincse@users.noreply.github.com>
2022-10-24 16:36:46 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* ==================================== Typing Animation ================================ */
- var typed = new Typed(".typing",{
-    strings:[""," an App Developer","a Web Developer","a Programmer","an Entrepreneur"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
- })
