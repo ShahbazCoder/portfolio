0d65004219c0f2414bcce03a96482ad833db8dd0
SHAHBAZ HUSSAIN <90305433+shahbazhussaincse@users.noreply.github.com>
2022-10-24 16:43:55 +0530

    Portfolio Android App
    Pursuing B.Tech in CSE (Computer Science & Engineering) working as a Campus Ambassador in E-Cell, IIT Bombay handling multiple projects right from planning, designing, implementing, testing to publishing . Possess a knack of applying analytical and problem-solving skills to meet clients varied demands within strict deadlines. Ensures timely completion of identified goals. Sound knowledge of C, Java and Python Programming Language as well as HTML5, CSS3, JavaScript for Front-End Development and Express, NodeJS, EJS for Back-End Development with its application on the Android Studio.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ==================================== Typing Animation ================================ */
+ var typed = new Typed(".typing",{
+    strings:[""," an App Developer","a Web Developer","a Programmer","an Entrepreneur"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+ })
